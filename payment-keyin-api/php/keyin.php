96d558ca599b19b70642d6e7fb8a82b7af0dc384
km-lee-ts <78412204+km-lee-ts@users.noreply.github.com>
2022-05-12 13:01:36 +0900

    initial commit

---
+++ payment-keyin-api/php/keyin.php
@@ -0,0 +1,86 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", true);
+
+
+$orderId = "billing-" . time()*1000;
+$amount = 50000;
+$cardNumber = "";
+$cardExpirationYear = "";
+$cardExpirationMonth = "";
+$cardPassword = "";
+$customerBirthday = "";
+$cardInstallmentPlan = 0
+$customerEmail = "customer@email.com";
+$customerName = "박토스";
+$orderName = "토스 키인 결제";
+
+$secretKey = 'test_ak_ZORzdMaqN3wQd5k6ygr5AkYXQGwy'; 
+
+$url = 'https://api.tosspayments.com/v1/payments/key-in';
+
+$data = ['orderId' => $orderId, 'amount' => $amount, 'cardNumber' => $cardNumber, 'cardExpirationYear' => $cardExpirationYear, 
+'cardExpirationMonth' => $cardExpirationMonth, 'cardPassword' => $cardPassword, 'customerBirthday' => $customerBirthday,
+'cardInstallmentPlan' => $cardInstallmentPlan, 'customerEmail' => $customerEmail, 'customerName' => $customerName,
+'orderName' => $orderName];
+
+$credential = base64_encode($secretKey . ':');
+
+$curlHandle = curl_init($url);
+
+
+curl_setopt_array($curlHandle, [
+    CURLOPT_POST => TRUE,
+    CURLOPT_RETURNTRANSFER => TRUE,
+    CURLOPT_HTTPHEADER => [
+        'Authorization: Basic ' . $credential,
+        'Content-Type: application/json'
+    ],
+    CURLOPT_POSTFIELDS => json_encode($data)
+]);
+
+
+
+
+
+$response = curl_exec($curlHandle);
+
+$httpCode = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE);
+$isSuccess = $httpCode == 200;
+$responseJson = json_decode($response);
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <title>결제 성공</title>
+    <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+</head>
+<body>
+<section>
+    <?php
+    if ($isSuccess) { ?>
+        <h1>결제 성공</h1>
+        <p>결과 데이터 : <?php echo json_encode($responseJson, JSON_UNESCAPED_UNICODE); ?></p>
+        <p>orderName : <?php echo $responseJson->orderName ?></p>
+        <p>method : <?php echo $responseJson->method ?></p>
+        <p>card -> number : <?php echo $responseJson->card->number ?></p>
+              
+        <?php
+    } else { ?>
+        <h1>결제 실패</h1>
+        <p>에러메시지 : <?php echo $responseJson->message ?></p>
+        <span>에러코드: <?php echo $responseJson->code ?></span>
+        <?php
+    }
+    ?>
+
+</section>
+</body>
+</html>
