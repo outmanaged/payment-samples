a31a8ce0c3a4c43c691f73938d4039813b401148
namwookim-2022 <namwoo.kim@namwoo.kim01>
2022-06-28 22:27:01 +0900

    update router file

--- payment-window/vue/src/router/index.js
+++ payment-window/vue/src/router/index.js
@@ -1,13 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import successView from '../views/successPage.vue'
-import appView from '../App.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'index',
-    component: appView
-  },
   {
     path: '/success',
     name: 'success',
